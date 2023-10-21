7755bd2e19f2fe4436e5fca244d19b529fd3a959
Caleb Diehl <cdiehl@ap.org>
2023-10-21 14:54:14 -0700

    add name data

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ui.css';
 
 function App() {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
+  const squareBox = React.useRef<HTMLInputElement>(undefined);
+  const actionBox = React.useRef<HTMLInputElement>(undefined);
+  const homeBox = React.useRef<HTMLInputElement>(undefined);
+
+  const [error, setError] = useState(false);
 
   const onReplace = () => {
+    const nameData = {
+      'Take action': actionBox.current.value,
+      'scooter button': squareBox.current.value,
+      'list #1': homeBox.current.value,
+    };
+    console.log(nameData);
     parent.postMessage({ pluginMessage: { type: 'replace-labels' } }, '*');
   };
 
@@ -26,9 +36,11 @@ function App() {
     <div>
       <h2>Rename your labels</h2>
       <label htmlFor="action-input">Action Button:</label>
-      <input id="action-input" type="text" ref={textbox}></input>
+      <input id="action-input" type="text" ref={actionBox}></input>
       <label htmlFor="square-input">Square Button:</label>
-      <input id="square-input" type="text" ref={textbox}></input>
+      <input id="square-input" type="text" ref={squareBox}></input>
+      <label htmlFor="home-input">Home Tile:</label>
+      <input id="home-input" type="text" ref={homeBox}></input>
       <div style={{ display: 'flex' }}>
         <button onClick={onReplace}>Replace Labels</button>
         <button onClick={onUndo}>Undo Changes</button>
